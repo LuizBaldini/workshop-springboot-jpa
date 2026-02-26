761c17003dd7b9b2110337a04eea36618a04be52
LuizBaldini <luizfelipebaldini.07@gmail.com>
2026-02-26 08:22:19 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -1,11 +1,9 @@
 package com.example.course.services;
 
-import com.example.course.entities.Order;
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 import java.util.Optional;
